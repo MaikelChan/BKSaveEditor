e71f8d44e4a4f43b3e1db2ac2d52285cd0972ac8
MaikelChan <maikelchan88@gmail.com>
2024-12-30 21:36:30 +0100

    Increased version.

--- src/Config.h
+++ src/Config.h
@@ -1,7 +1,7 @@
 #pragma once
 
 #define PROJECT_NAME "BKSaveEditor"
-#define PROJECT_VER  "1.0.1"
+#define PROJECT_VER  "1.0.2"
 #define PROJECT_VER_MAJOR "1"
 #define PROJECT_VER_MINOR "0"
-#define PTOJECT_VER_PATCH "1"
+#define PTOJECT_VER_PATCH "2"
