7a745699e297a3494989187ff758a881dffc6a46
MaikelChan <maikelchan88@gmail.com>
2026-04-09 15:27:13 +0200

    Updated version and Windows resource.

--- src/Config.h
+++ src/Config.h
@@ -1,9 +1,9 @@
 #pragma once
 
 constexpr const char* PROJECT_NAME = "BKSaveEditor";
-constexpr const char* PROJECT_VER = "1.1.0";
+constexpr const char* PROJECT_VER = "1.1.1";
 constexpr const char* PROJECT_VER_MAJOR = "1";
 constexpr const char* PROJECT_VER_MINOR = "1";
-constexpr const char* PTOJECT_VER_PATCH = "0";
+constexpr const char* PTOJECT_VER_PATCH = "1";
 
 constexpr const char* SIMPLEINI_COMMIT_HASH = "18fd1db";
